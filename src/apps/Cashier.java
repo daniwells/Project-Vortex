0b0b3360d724ee7073c47ea22da8cc0f35ddf230
Levi <levi.tobio2505.com>
2024-07-15 11:55:14 -0300

    tela de registro

--- src/apps/Cashier.java
+++ src/apps/Cashier.java
@@ -33,11 +33,11 @@ public class Cashier extends javax.swing.JDialog {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1086, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 640, Short.MAX_VALUE)
         );
 
         pack();
